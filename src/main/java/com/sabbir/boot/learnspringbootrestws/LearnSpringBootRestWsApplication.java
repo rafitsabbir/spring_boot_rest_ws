290b564d0c6f5511cd7a4123194cd0b7271bcdd5
rafitsabbir <rafitsabbir@gmail.com>
2022-12-15 01:20:47 +0600

    initial project structure

---
+++ src/main/java/com/sabbir/boot/learnspringbootrestws/LearnSpringBootRestWsApplication.java
@@ -0,0 +1,13 @@
+package com.sabbir.boot.learnspringbootrestws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootRestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootRestWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sabbir/boot/learnspringbootrestws/LearnSpringBootRestWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabbir.boot.learnspringbootrestws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootRestWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
