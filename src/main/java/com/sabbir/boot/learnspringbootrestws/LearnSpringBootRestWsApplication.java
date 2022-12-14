f4db1a99d6706ad93353d07b8990203266f274d5
rafitsabbir <rafitsabbir@gmail.com>
2022-12-15 02:16:04 +0600

    03-03 tightly coupled example.

--- src/main/java/com/sabbir/boot/learnspringbootrestws/LearnSpringBootRestWsApplication.java
+++ src/main/java/com/sabbir/boot/learnspringbootrestws/LearnSpringBootRestWsApplication.java
@@ -3,11 +3,18 @@ package com.sabbir.boot.learnspringbootrestws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sabbir.boot.learnspringbootrestws.game.GameRunner;
+import com.sabbir.boot.learnspringbootrestws.game.MarioGame;
+
 @SpringBootApplication
 public class LearnSpringBootRestWsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringBootRestWsApplication.class, args);
+		// SpringApplication.run(LearnSpringBootRestWsApplication.class, args);
+		MarioGame mario = new MarioGame();
+		GameRunner runner = new GameRunner(mario);
+		runner.run();
+
 	}
 
 }
---
+++ src/main/java/com/sabbir/boot/learnspringbootrestws/game/GameRunner.java
@@ -0,0 +1,19 @@
+package com.sabbir.boot.learnspringbootrestws.game;
+
+public class GameRunner {
+
+	private MarioGame game;
+
+	public GameRunner(MarioGame game) {
+		this.game = game;
+	}
+
+	public void run() {
+		game.up();
+		game.down();
+		game.left();
+		game.right();
+
+	}
+
+}
